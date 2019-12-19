0be1898349730988d48d6d1b53ef9e776d1d2881
=elirehema <=rocksandystone@gmail.com>
2019-12-19 15:47:06 +0300

    Changed Calculator layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,39 @@
 <template>
   <v-app dark class="app">
     <v-navigation-drawer  v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">  <v-img
-        class="mx-1"
-        :src="require('~/assets/images/logo_gold.png')"
-        max-height="80"
-        max-width="80"
-        contain
-      ></v-img></v-list-item-title>
-          <v-list-item-subtitle>{{ $t('label.message.motto') }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+     
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
 
-      <v-divider></v-divider>
-      <v-list nav dense :shaped="shaped">
-        <v-list-item-group>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">Your Name</v-list-item-title>
+            <v-list-item-subtitle>youremail@example.com</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-list subheader shaped >
+        
           <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
+            <v-list-item-avatar>
+              <v-icon
+              :class="[item.iconClass]"
+              v-text="item.icon"></v-icon>
+            </v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title v-text="item.title" />
-              <v-list-item-subtitle v-text="item.subtitle" />
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+              <v-list-item-subtitle  >{{ item.subtitle }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
-        </v-list-item-group>
-        <v-list-item-group>
-          <v-list-item></v-list-item>
-        </v-list-item-group>
+        
       </v-list>
     </v-navigation-drawer>
     <v-app-bar elevation="1" :clipped-left="clipped" fixed app :collapse-on-scroll="collapseOnScroll">
@@ -61,7 +65,7 @@
         contain
       ></v-img>
       <v-spacer />
-      <v-list v-if="$vuetify.breakpoint.smAndUp">
+     <!-- <v-list v-if="$vuetify.breakpoint.smAndUp">
         <v-list-item>
           <v-btn
           small
@@ -73,7 +77,7 @@
           text
         >{{item.title}}</v-btn>
         </v-list-item>
-      </v-list>
+      </v-list>-->
     </v-app-bar>
     <v-content>
       <v-container>
@@ -108,25 +112,36 @@ export default {
           icon: "mdi-view-dashboard",
           title: "Home",
           subtitle: "Go to Home Page",
-          to: "/homepage"
+          to: "/homepage",
+           iconClass: 'blue white--text', 
         },
         {
-          icon: "mdi-help-circle",
+          icon: "mdi-help-circle-outline",
           title: "Help",
           subtitle: "Are you in need of help ?",
-          to: "/help"
+          to: "/help",
+          iconClass: 'info white--text'
         },
         {
           icon: "mdi-information-variant",
           title: "About Us",
           subtitle: "Read more about us",
-          to: "/about"
+          to: "/about",
+          iconClass: 'info white--text'
         },
         {
-          icon: "mdi-phone-classic",
+          icon: "mdi-deskphone",
           title: "Contact Us",
           subtitle: "Contact kopasmart team/community",
-          to: "/contacts"
+          to: "/contacts",
+          iconClass: 'info white--text'
+        },
+         {
+          icon: "mdi-calculator",
+          title: "Calculator",
+          subtitle: "Repayment Schedule generator",
+          to: "/calculator",
+          iconClass: 'info white--text'
         }
       ],
       miniVariant: false,
