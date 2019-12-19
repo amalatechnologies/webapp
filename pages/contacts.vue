a413eb2f233ecbd55d106320b4b37fe64256c879
=elirehema <=rocksandystone@gmail.com>
2019-12-19 18:32:17 +0300

    Contact Us form

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,43 +1,110 @@
 <template>
   <v-app class="app">
     <v-container fluid>
-      <v-card 
-      class="mx-auto">
-        <v-card-title>Contact Us</v-card-title>
-        <v-card-subtitle>Tell us your views </v-card-subtitle>
-        <div class="padding">
-            <v-form ref="form" v-model="valid" :lazy-validation="lazy">
-          <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+      
+      <v-row justify="center">
+    <v-col cols="12" sm="10" md="8" lg="6">
+      <v-card ref="form" ripple>
+        <v-card-text>
+          <v-text-field
+            ref="name"
+            v-model="name"
+            :rules="[() => !!name || 'This field is required']"
+            :error-messages="errorMessages"
+            label="Full Name"
+            placeholder="John Doe"
+            required
+          ></v-text-field>
 
-          <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+          <v-text-field
+           v-model="email"
+           :rules="emailRules" 
+           label="E-mail"
+            placeholder="youremaill@example.com" 
+            required>
+            </v-text-field>
 
           <v-text-field
             v-model="message"
             :rules="emailRules"
-            label="What is your Mesage ?"
+            label="Message"
+            placeholder=" Lorem ipsum dolor sit de amet ... "
             required
-            outlined
           ></v-text-field>
-
-          <v-select
-            v-model="select"
-            :items="items"
-            :rules="[v => !!v || 'Item is required']"
-            label="Item"
+          <v-text-field
+            ref="address"
+            v-model="address"
+            :rules="[
+              () => !!address || 'This field is required',
+              () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
+              addressCheck
+            ]"
+            label="Address Line"
+            placeholder="Snowy Rock Pl"
+            counter="25"
             required
-          ></v-select>
-
-          <v-checkbox
-            v-model="checkbox"
-            :rules="[v => !!v || 'You must agree to continue!']"
-            label="Do you agree?"
+          ></v-text-field>
+          <v-text-field
+            ref="city"
+            v-model="city"
+            :rules="[() => !!city || 'This field is required', addressCheck]"
+            label="City"
+            placeholder="El Paso"
             required
-          ></v-checkbox>
-
-          <v-btn color="secondary" class="mr-4" outlined small @click="validate">Send Message</v-btn>
-        </v-form>
-        </div>
+          ></v-text-field>
+          <v-text-field
+            ref="state"
+            v-model="state"
+            :rules="[() => !!state || 'This field is required']"
+            label="State/Province/Region"
+            required
+            placeholder="TX"
+          ></v-text-field>
+          <v-text-field
+            ref="zip"
+            v-model="zip"
+            :rules="[() => !!zip || 'This field is required']"
+            label="ZIP / Postal Code"
+            required
+            placeholder="79938"
+          ></v-text-field>
+          <v-autocomplete
+            ref="country"
+            v-model="country"
+            :rules="[() => !!country || 'This field is required']"
+            :items="countries"
+            label="Country"
+            placeholder="Select..."
+            required
+          ></v-autocomplete>
+        </v-card-text>
+        <v-divider class="mt-12"></v-divider>
+        <v-card-actions>
+          <v-btn text>Cancel</v-btn>
+          <v-spacer></v-spacer>
+          <v-slide-x-reverse-transition>
+            <v-tooltip
+              v-if="formHasErrors"
+              left
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  class="my-0"
+                  @click="resetForm"
+                  v-on="on"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </template>
+              <span>Refresh form</span>
+            </v-tooltip>
+          </v-slide-x-reverse-transition>
+          <v-btn color="primary" text @click="submit">Submit</v-btn>
+        </v-card-actions>
       </v-card>
+    </v-col>
+  </v-row>
     </v-container>
   </v-app>
 </template>
@@ -65,7 +132,16 @@ export default {
     select: null,
     items: ["Item 1", "Item 2", "Item 3", "Item 4"],
     checkbox: false,
-    lazy: false
+    lazy: false,
+     countries: ['Tanzania','Kenya','Uganda','Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
+      errorMessages: '',
+      name: null,
+      address: null,
+      city: null,
+      state: null,
+      zip: null,
+      country: null,
+      formHasErrors: false,
   }),
 
   methods: {
@@ -80,6 +156,18 @@ export default {
     resetValidation() {
       this.$refs.form.resetValidation();
     }
+  },
+  computed:{
+    form () {
+        return {
+          name: this.name,
+          address: this.address,
+          city: this.city,
+          state: this.state,
+          zip: this.zip,
+          country: this.country,
+        }
+      },
   }
 };
 </script>
