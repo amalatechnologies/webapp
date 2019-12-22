d332a37950f212033565b929c9eef5e48e310818
=elirehema <=rocksandystone@gmail.com>
2019-12-22 16:34:27 +0300

    Help Page

--- static/js/help.js
+++ static/js/help.js
@@ -5,31 +5,37 @@ export default {
           {
             color: "success",
             help: "1",
+            subtitle: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque",
             title: "How does it work ?"
           },
           {
             color: "primary",
             help: "1",
-            title: "Another very important questions in FAQ ?"
-          },
-          {
-            color: "secondary",
-            help: "1",
-            title: "Another very important questions in FAQ ?"
-          },
-          {
-            color: "error",
-            help: "1",
+            subtitle: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque",
             title: "Another very important questions in FAQ ?"
           },
           {
             color: "info",
             help: "1",
+            subtitle: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque",
             title: "Another very important questions in FAQ ?"
           },
           {
-            color: "warning",
+            color: "success",
             help: "1",
+            subtitle: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque",
+            title: "Another very important questions in FAQ ?"
+          },
+          {
+            color: "primary",
+            help: "1",
+            subtitle: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque",
+            title: "Another very important questions in FAQ ?"
+          },
+          {
+            color: "info",
+            help: "1",
+            subtitle: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque",
             title: "The standard Lorem Ipsum passage, used since the 1500s ?"
           },
      
