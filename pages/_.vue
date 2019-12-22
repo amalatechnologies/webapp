5c44c88fa2781ed0215a6383fd751a9e8a7907d6
=elirehema <=rocksandystone@gmail.com>
2019-12-22 15:45:14 +0300

    Page Not found

---
+++ pages/_.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app class="app">
+    <v-container fluid>
+      <v-card class="mx-auto center">
+        <v-img :src="require('~/assets/images/404.png')">
+          <v-btn class="center info" to="/">Go back Home</v-btn>
+        </v-img>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: "home",
+  data() {
+    return {};
+  },
+  head() {
+    return {
+      title: "Page Not Found"
+    };
+  }
+};
+</script>
+<style scoped>
+.center {
+  left: 50%;
+  top: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+}
+</style>
