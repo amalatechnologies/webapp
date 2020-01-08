387700fdb9cab282a5736ba08a137ad761349ca3
=elirehema <=rocksandystone@gmail.com>
2020-01-08 19:34:20 +0300

    Added Repayment Schedule Hist

--- static/js/about.js
+++ static/js/about.js
@@ -1,14 +1,14 @@
 export default {
-    data(){
-        return{
-            title: 'About Us',
-            app_name: 'Kopasmart Application',
-            
-        };
-    },
-    head () {
-        return{
-          title: 'About Us'
-        }
-      },
-}
+  data() {
+    return {
+      title: 'About Us',
+      app_name: 'Kopasmart Application',
+
+    };
+  },
+  head() {
+    return {
+      title: 'About Us'
+    };
+  },
+};
