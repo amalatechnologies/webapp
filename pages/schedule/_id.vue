60e55c07c059c6a43f3747c5d868039206f7af5b
=elirehema <=rocksandystone@gmail.com>
2020-01-09 16:14:23 +0300

    Added View Repayment Schedule

---
+++ pages/schedule/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app class="app">
+    <v-container>
+      <v-card flat>
+        <v-container fluid></v-container>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+<script lang="js" src="~/static/js/id_schedule.js"></script>
