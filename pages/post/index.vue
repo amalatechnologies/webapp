38315bda24c2736a02dd40895e2306a451439de1
lukeloNgajilo <ngajilol@gmail.com>
2021-01-06 23:43:11 +0300

    post on walls fixed

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,8 @@
 <template>
   <v-app class="app">
-
     <v-container fluid pa-0 ma-0>
-      <v-card
-        class="mx-auto pa-0 ma-0"
-        flat>
-        <v-form
-          ref="form"
-          v-model="form"
-          class="pa-4 pt-6"
-        >
+      <v-card class="mx-auto pa-0 ma-0" flat>
+        <v-form ref="form" v-model="form" class="pa-4 pt-6">
           <v-textarea
             v-model="text_content"
             auto-grow
@@ -21,24 +14,17 @@
           ></v-textarea>
         </v-form>
         <v-card-actions>
-          <v-btn
-            outlined
-            class="accent white--text"
-            @click="$refs.form.reset()"
-          >
-            Clear
-          </v-btn>
+          <v-btn outlined class="accent white--text" @click="$refs.form.reset()">Clear</v-btn>
           <v-spacer></v-spacer>
           <v-btn
             :disabled="!form"
             :loading="isLoading"
             class="white--text"
-
             color="deep-purple accent-4"
             depressed
             @click.stop="createNewPost()"
-          >Submit
-          </v-btn>
+            to="/home"
+          >Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-container>
@@ -51,20 +37,27 @@ export default {
       form: false,
       dialog: false,
       isLoading: false,
-        text_content:null,
+      text_content: null,
       rules: {
-        email: v => !!(v || '').match(/@/) || 'Please enter a valid email',
-        length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
-        password: v => !!(v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
-          'Password must contain an upper case letter, a numeric character, and a special character',
-        required: v => !!v || 'This field is required',
+        email: (v) => !!(v || "").match(/@/) || "Please enter a valid email",
+        length: (len) => (v) =>
+          (v || "").length >= len ||
+          `Invalid character length, required ${len}`,
+        password: (v) =>
+          !!(v || "").match(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/
+          ) ||
+          "Password must contain an upper case letter, a numeric character, and a special character",
+        required: (v) => !!v || "This field is required",
       },
-    }
+    };
   },
-  methods:{
-    createNewPost(){
-      this.$store.dispatch('postNewBlogPosts',{"text_content": this.text_content} )
-    }
-  }
-}
+  methods: {
+    createNewPost() {
+      this.$store.dispatch("postNewBlogPosts", {
+        text_content: this.text_content,
+      });
+    },
+  },
+};
 </script>
