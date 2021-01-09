861169382c33a27b3da25bee1b5b195363da1bff
lukeloNgajilo <ngajilol@gmail.com>
2021-01-10 01:40:02 +0300

    searching

---
+++ static/js/search.js
@@ -0,0 +1,25 @@
+export default {
+  data: () => ({
+    search: "",
+    searcheditems: []
+  }),
+
+  methods: {
+    searches() {
+      let search = "";
+      search = this.search;
+
+      this.$store.dispatch("searching", search);
+    },
+  },
+
+  computed: {
+    searcheditem() {
+
+      //  console.log(this.$store.getters.searcheditems);
+      return this.$store.getters.getitems;
+
+
+    }
+  },
+};
