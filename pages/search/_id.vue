f2ffa52131a52bd7eaf6f3d0db67aa987e1bbe26
lukeloNgajilo <ngajilol@gmail.com>
2021-02-09 01:27:01 +0300

    search complete

---
+++ pages/search/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nuxt-child :searcheditem="searcheditem" />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    error() {
+      //  console.log(this.$store.getters.searcheditems);
+      return this.$store.getters.error;
+    },
+    searcheditem() {
+      //  console.log(this.$store.getters.searcheditems);
+      return this.$store.getters.getitems;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ pages/search/_id/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-card class="mx-auto" max-width="344">
+      <v-card-text>
+        <div class="text--primary">Users</div>
+        <div>Username: {{searcheditem[0].username}}</div>
+        <div>Email: {{searcheditem[0].email}}</div>
+        <div>Role: {{searcheditem[0].role}}</div>
+        <div>Lender: {{searcheditem[0].is_lender}}</div>
+        <div>Borrower: {{searcheditem[0].is_borrower}}</div>
+        <div>is_active: {{searcheditem[0].is_active}}</div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["searcheditem"],
+};
+</script>
+
+<style>
+</style>
