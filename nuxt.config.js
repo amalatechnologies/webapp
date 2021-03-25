041d174b884760a96cf7a11604b327103a08e525
Elirehema <rocksandystone@gmail.com>
2021-03-25 11:21:03 +0300

    update router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   router: {
-    base: routerBase,
+    base: '/',
     routerNameSplitter: '/',
   },
   env: {
