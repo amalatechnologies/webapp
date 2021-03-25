0647b23ae67168fd89df0cd0d26c04ccc287b4bb
Elirehema <rocksandystone@gmail.com>
2021-03-25 13:44:00 +0300

    Data store impression

--- plugins/vuepersistence.js
+++ plugins/vuepersistence.js
@@ -5,12 +5,13 @@ const ls = new SecureLS({ isCompression: false });
 export default ({ store }) => {
   window.onNuxtReady(() => {
     createPersistedState({
-      paths: ["posts", "profile", "repayments"],
+      paths: ["posts", "profile", "repayments","schedules"],
       storage: {
         getItem: key => ls.get(key),
         setItem: (key, value) => ls.set(key, value),
         removeItem: key => ls.remove(key)
       }
+      
       
     })(store);
   });
