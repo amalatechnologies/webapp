5fdcd52a5fb2199c8918a5fec082bf5ffaa7ccde
elirehema <29277048+elirehema@users.noreply.github.com>
2022-10-07 08:05:44 +0000

    Deploying to gh-pages from  @ 8f4228a29dba18e244c3b0ce52e8126b112e060d 🚀

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,8 @@
 export default {
   meta: {
     auth: {
-      requiresAuth: false
-    }
+      requiresAuth: false,
+    },
   },
   layout: "signup",
   data: () => ({
@@ -17,44 +17,41 @@ export default {
     items: [
       {
         name: "Lender",
-        value: "lender"
+        value: "lender",
       },
       {
         name: "Borrower",
-        value: "borrower"
-      }
+        value: "borrower",
+      },
     ],
-    role:null,
+    role: null,
     isLoading: false,
     password: undefined,
     username: undefined,
     rePassword: undefined,
     rules: {
-      email: v => (v || "").match(/@/) || "Please enter a valid email",
-      length: len => v => (v || "").length >= len || `Invalid character length, required ${len}`,
-      password: v =>
+      email: (v) => (v || "").match(/@/) || "Please enter a valid email",
+      length: (len) => (v) =>
+        (v || "").length >= len || `Invalid character length, required ${len}`,
+      password: (v) =>
         (v || "").match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
         "Password must contain an upper case letter, a numeric character, and a special character",
-      required: v => !!v || "This field is required"
-    }
+      required: (v) => !!v || "This field is required",
+    },
   }),
-  computed: {
-
-  },
+  computed: {},
   methods: {
     signUpWithCredentials() {
       let data = {
         username: this.username,
         password: this.password,
         email: this.email,
-        role:this.role
-
+        role: this.role,
       };
-      console.log(data.role);
+      // console.log(data.role);
       if (this.password === this.rePassword) {
-        this.$store.dispatch('register', data);
+        this.$store.dispatch("register", data);
       }
-    }
-
-  }
+    },
+  },
 };
